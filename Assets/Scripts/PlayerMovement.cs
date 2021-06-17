c0b685ba29568951c82063a54ce7e1b8e90fc1d6
LAPTOP-U6F0A5V5\shubh <shubhankarsharma0135@gmail.com>
2021-06-18 00:28:24 +0530

    Added Player Movement Script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float forwardSpeed;
+    public float Sensitivity;
+    public float rotationSensitivity;
+
+    private Touch touch;
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * forwardSpeed * Time.deltaTime);
+
+        sideWaysMovement();
+    }
+
+    void sideWaysMovement()
+    {
+        if(Input.touchCount > 0)
+        {
+            touch = Input.GetTouch(0);
+
+            if(touch.phase == TouchPhase.Moved)
+            {
+                // Making player Move sideways on touch
+                float xSlide = transform.position.x + touch.deltaPosition.x * Sensitivity;
+                transform.position = new Vector3(xSlide, transform.position.y, transform.position.z);
+
+                // Rotating the player based on sidewaysMovement
+                transform.Rotate(Vector3.up * touch.deltaPosition.x / ( rotationSensitivity * 10 + 10));
+            }
+        }
+    }
+}
