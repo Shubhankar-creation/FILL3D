d28a04b36cb5bcfeabf3f24ff43d19301f6a4af5
LAPTOP-U6F0A5V5\shubh <shubhankarsharma0135@gmail.com>
2021-06-26 23:44:37 +0530

    added crown game object

--- Assets/Scripts/DetectShapes.cs
+++ Assets/Scripts/DetectShapes.cs
@@ -1,25 +1,36 @@
 using UnityEngine;
-
+using UnityEngine.UI;
 public class DetectShapes : MonoBehaviour
 {
     private float iniScaleX;
-
+    public int sliderVal;
+    public Slider slider;
+    public float fillSpeed;
     private void Start()
     {
         iniScaleX = transform.localScale.x;
     }
 
+    private void Update()
+    {
+        if (slider.value < sliderVal)
+        {
+            slider.value = sliderVal * fillSpeed * Time.deltaTime;
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("AllyShape"))
         {
             IncShapeVol(other);
             Destroy(other.gameObject);
+            sliderVal += 1;
         }
         else if(other.gameObject.CompareTag("EnemyShape"))
         {
             DecShapeVol(other);
             Destroy(other.gameObject);
+            if(slider.value > 1) sliderVal = -1;
         }
     }
 
---
+++ Assets/Scripts/rotateCrown.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateCrown : MonoBehaviour
+{
+    public float rotationSpeed;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+}
